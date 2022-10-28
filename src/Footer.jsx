415168bab9da14dcca6c1a3b2903c8afa844050f
mided3v <ayo4real6@gmail.com>
2022-10-28 16:32:31 +0100

    first commit

---
+++ src/Footer.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import ZuriLogo from './asset/Vector.png'
+import HNG from './asset/Footer text.png'
+import I4G from './asset/I4G.png'
+import FooterStyles from './Footer.css'
+
+export const Footer = () => {
+  return (
+      <div id="footer">
+          
+      <a href="https://training.zuri.team/">
+        <img src={ZuriLogo} alt="zuri internship" />
+      </a>
+      <a href="https://training.zuri.team/">
+        <img src={HNG} alt="hng" />
+      </a>
+      <a href="https://training.zuri.team/">
+        <img src={I4G} alt="i4g" />
+      </a>
+    </div>
+  );
+}
+
+export default Footer
---
+++ src/Header.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import DP from './asset/profile__img.png'
+import { BsTwitter } from 'react-icons/bs'
+import { AiOutlineSlack} from 'react-icons/ai'
+import HeaderStyles from './Header.css'
+
+export const Header = () => {
+    return (
+      <>
+        <div className="profile_ctn">
+          <img src={DP} alt="profile img" id="profile_img" />
+          <h1 className="user_name">Annie Black</h1>
+          <a href="www.twitter.com" id="twitter">
+            <BsTwitter />{" "}
+          </a>
+          <a href="www.twitter.com" id="slack">
+            <AiOutlineSlack />{" "}
+          </a>
+        </div>
+      </>
+    );
+}
