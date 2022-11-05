fe1aa1142f4ea12e85d4f45204945428acaedf8f
mided3v <ayo4real6@gmail.com>
2022-11-06 00:13:05 +0100

    docs: removed unused docs from the header

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,19 +1,18 @@
-import React from 'react'
-import DP from '../asset/profile__img.png'
-import { BsTwitter } from 'react-icons/bs'
-import { AiOutlineSlack} from 'react-icons/ai'
-import HeaderStyles from './Header.css'
-import Slack from '../asset/slack.png'
+import React from "react";
+import DP from "../asset/profile__img.png";
+
+import HeaderStyles from "./Header.css";
+import Slack from "../asset/slack.png";
 
 export const Header = () => {
-    return (
-      <>
-        <div className="profile_ctn">
-          <a href="/">
-            <img src={DP} alt="profile img" id="profile_img" />
-          </a>
-          <h1 className="user_name">Annie Black</h1>
-        </div>
-      </>
-    );
-}
+  return (
+    <>
+      <div className="profile_ctn">
+        <a href="/">
+          <img src={DP} alt="profile img" id="profile_img" />
+        </a>
+        <h1 className="user_name">Annie Black</h1>
+      </div>
+    </>
+  );
+};
