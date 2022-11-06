8b9787dae5351e22d9ea81aa2b457227de0fd3ca
mided3v <ayo4real6@gmail.com>
2022-11-07 00:58:04 +0100

    feat: updated the share buttons

--- src/header/Header.jsx
+++ src/header/Header.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import DP from "../asset/profile__img.png";
+import Share from '../asset/_Avatar share button.png'
+import Share2 from '../asset/_Avatar share button (1).png'
 
 import HeaderStyles from "./Header.css";
 import Slack from "../asset/slack.png";
@@ -10,6 +12,10 @@ export const Header = () => {
       <div className="profile_ctn">
         <a href="/">
           <img src={DP} alt="profile img" id="profile_img" />
+        </a>
+        <a href="/">
+          <img src={Share} alt="share_btn" id="share_btn"/>
+          <img src={Share2} alt="share_btn" id="share_btn2"/>
         </a>
         <h1 className="user_name">Annie Black</h1>
       </div>
